39e4aaa0ac61876c049aa2da522a60e1411b1212
TheHari <hhari9282@gmail.com>
2023-12-04 13:28:14 +0530

    second commit

--- infoxfinsys/settings.py
+++ infoxfinsys/settings.py
@@ -140,12 +140,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'altostechnologies6@gmail.com'
-EMAIL_HOST_PASSWORD = 'jkdpqggohjsmhyay'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER='hhari9282@gmail.com'
+EMAIL_HOST_PASSWORD='axtginknxufhaodn'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
